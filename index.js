7e2e61db5ff0df7f5cd1ef5f13f1322c895fee25
SteTisci <ste.tisci@gmail.com>
2024-05-30 19:40:09 +0200

    aggiunta classi html dinamico per css

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const URL = "https://dati.comune.milano.it/dataset/d01a4abd-1e76-4b06-a9e6-e96a4f1d6a35/resource/ac9a644c-a2bf-419a-9b93-735417eb6c73/download/portale_opendata_daterangemonth_20240510.json";
 
+// Manda una richiesta al sito OpenData
 const fetchData = async (URL) => {
     try {
         const response = await fetch(URL);
@@ -16,6 +17,7 @@ const fetchData = async (URL) => {
     };
 }
 
+// Formattazione dati da Risposta Json
 const dataExtraction = (data) => {
     const info = data.map(element => {
         return {
@@ -29,33 +31,31 @@ const dataExtraction = (data) => {
     return info;
 }
 
-// problema dati visualiizati causato da select querySelector, non prende value perche sono in option 
-// DA SISTEMARE STO BORDELLO
-
-const showInfo = (info) => {
-    
-    document.querySelector('.choice').forEach(input => {
-        input.addEventListener('change', () => {
-            const div = document.querySelector('.info');
-            const yearValue = document.querySelector('.year').value;
-            const monthValue = document.querySelector('.month').value;
-            info.map(element => {
-                if (element.year == year && element.month == month) {
-                    div.innerHTML = `
-                        <p>Visite data ${element.year}/${element.month}</p>
-                        <p>Visualizzazioni: ${element.visits}</p>
-                        <p>Pagine Visitate: ${element.pageViews}</p>
-                        <p>Visitatori: ${element.visitors}</p>
-                    `;
-                };
-            });
-        });
-    });
-}
-
 fetchData(URL)
     .then((data) => {
         const info = dataExtraction(data);
-        showInfo(info);
+
+        document.querySelector('.send').addEventListener('click', () => {
+
+            const div = document.querySelector('.info');
+            const yearValue = document.querySelector('.yearChoice').value;
+            const monthValue = document.querySelector('.monthChoice').value;
+
+            div.innerHTML = '';  // Clear previous results
+
+            info.forEach(element => {
+
+                if (element.year == yearValue && element.month == monthValue) {
+                    div.innerHTML = `
+                        <div class="result">
+                            <p class="data"><strong>Data:</strong> ${element.month}/${element.year}</p>
+                            <p class="visits"><strong>Visite:</strong> ${element.visits}</p>
+                            <p class="pageViews"><strong>Pagine Visitate:</strong> ${element.pageViews}</p>
+                            <p class="visitors"><strong>Visitatori:</strong> ${element.visitors}</p>
+                        </div>
+                    `
+                }
+            })
+        });
     })
-// prendere i valori Select html e mostrare info in base mese e anno
+    .catch(error => console.error(error));
