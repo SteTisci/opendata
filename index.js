11292e48b3bad6956e9830fc9cfcee51391f609d
SteTisci <ste.tisci@gmail.com>
2024-06-01 04:44:24 +0200

    Miglioramento struttura codice

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+// Url per dati formato Json sito OpenData
 const URL = "https://dati.comune.milano.it/dataset/d01a4abd-1e76-4b06-a9e6-e96a4f1d6a35/resource/ac9a644c-a2bf-419a-9b93-735417eb6c73/download/portale_opendata_daterangemonth_20240510.json";
 
 
-// Manda una richiesta al sito OpenData
+// Invio richiesta al sito OpenData
 const fetchData = async (URL) => {
     try {
         const response = await fetch(URL);
 
         // Controllo stato risposta
         if (!response.ok) {
-            throw new Error(`Errore durante la richiesta ${response.statusText}`);
+            throw new Error(`Errore durante la richiesta - ${response.statusText}`);
         }
+        // Estrazione body da risposta
         const data = await response.json();
         
         return data;
@@ -22,7 +24,7 @@ const fetchData = async (URL) => {
 
 
 // Formattazione dati da Risposta Json
-const dataExtraction = (data) => {
+const getData = (data) => {
     const info = data.map(element => {
         return {
             year: element.anno,
@@ -36,97 +38,107 @@ const dataExtraction = (data) => {
 }
 
 
+// Inserimento dati nella pagina html
+const insertDataHTMl = (info, div) => {
+
+    const yearValue = document.querySelector('.yearChoice').value;
+    const monthValue = document.querySelector('.monthChoice').value;
+
+    div.style.backgroundColor = '#e9ecef';
+    div.innerHTML = '';  // Rimuove risultati precedenti
+
+    // Cerco l'elemento con la data corrispondente all'input dell'utente
+    const filteredData = info.find(element => element.year == yearValue && element.month == monthValue);
+        
+    // Se l'elemento esiste viene inserito nella pagina html
+    if (filteredData) {
+        // Struttura elementi che verranno inseriti
+        div.innerHTML = `
+            <div class="result">
+                <p class="visits"><strong>Visite:</strong> ${filteredData.visits}</p>
+                <p class="pageViews"><strong>Pagine Visitate:</strong> ${filteredData.pageViews}</p>
+                <p class="visitors"><strong>Visitatori:</strong> ${filteredData.visitors}</p>
+            </div>
+        `
+    } else {
+        // Struttura elementi se non sono presenti dati
+        div.innerHTML = `
+            <div class='result'>
+                <p class='noData'>Nessun dato presente per la Data ${monthValue}/${yearValue}</p>
+            </div>    
+        `   
+    }
+}
+
+
+// Funzione per la creazione di un grafico con i dati Open Data
+const createChart = (info) => {
+
+    const xValues = info.map(({ month, year }) => `${month}/${year}`);
+    const visitors = info.map(({ visitors }) => visitors);
+    const visits = info.map(({visits}) => visits);
+    const pageviews = info.map(({pageViews}) => pageViews);
+
+    new Chart("myChart", {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [{
+                label: "Visitatori",
+                backgroundColor: "rgba(0, 0, 0, 0)",
+                borderColor: "rgba(0, 0, 255, 1)",
+                data: visitors,
+                fill: false,
+                tension: 0.1, // rende la linea leggermente curva
+            }, 
+            {
+                label: "Visite",
+                backgroundColor: "rgba(0, 0, 0, 0)",
+                borderColor: "rgba(0, 255, 0, 1)",
+                data: visits,
+                fill: false,
+                tension: 0.1,
+            },
+            {
+                label: "Pagine visitate",
+                backgroundColor: "rgba(0, 0, 0, 0)",
+                borderColor: "rgba(255, 0, 0, 1)",
+                data: pageviews,
+                fill: false,
+                tension: 0.1,
+            }],
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Visitors Over Time'
+                },
+                legend: {
+                    display: true,
+                    position: 'top'
+                },
+                tooltip: {
+                    mode: 'index',
+                    intersect: false,
+                },
+            },
+        },
+    });
+}
+
+
 fetchData(URL)
     .then((data) => {
-        const info = dataExtraction(data);
+        const info = getData(data);
 
-        // Event listener per la visualizzazione dei risultati quandi il button send viene cliccato
+        // Event listener per la visualizzazione dei risultati quando il button send viene cliccato
         document.querySelector('.send').addEventListener('click', () => {
-
             const div = document.querySelector('.info');
-            const yearValue = document.querySelector('.yearChoice').value;
-            const monthValue = document.querySelector('.monthChoice').value;
-
-            div.innerHTML = '';  // Rimuove risultati precedenti
-
-            // Cerco l'elemento con la data corrispondente all'input dell'utente
-            const filteredData = info.find(element => element.year == yearValue && element.month == monthValue);
-                
-            // Se l'elemento esiste viene inserito nella pagina html
-            if (filteredData) {
-                // Struttura elementi che verranno inseriti
-                div.innerHTML = `
-                    <div class="result">
-                        <p class="data"><strong>Data:</strong> ${filteredData.month}/${filteredData.year}</p>
-                        <p class="visits"><strong>Visite:</strong> ${filteredData.visits}</p>
-                        <p class="pageViews"><strong>Pagine Visitate:</strong> ${filteredData.pageViews}</p>
-                        <p class="visitors"><strong>Visitatori:</strong> ${filteredData.visitors}</p>
-                    </div>
-                `
-            } else {
-                div.innerHTML = `
-                    <div class='result'>
-                        <p class='noData'>Nessun dato presente per la Data ${monthValue}/${yearValue}</p>
-                    </div>    
-                `   
-            }
+            insertDataHTMl(info, div);
         });
-        return info;
-    })
-    .then((info) => {
-        const reversedInfo = info.reverse();
-
-        const xValues = reversedInfo.map(({ month, year }) => `${month}/${year}`);
-        const visitors = reversedInfo.map(({ visitors }) => visitors);
-        const visits = reversedInfo.map(({visits}) => visits);
-        const pageviews = reversedInfo.map(({pageViews}) => pageViews);
-
-        new Chart("myChart", {
-            type: "line",
-            data: {
-                labels: xValues,
-                datasets: [{
-                    label: "Visitatori",
-                    backgroundColor: "rgba(0, 0, 0, 0)",
-                    borderColor: "rgba(0, 0, 255, 1)",
-                    data: visitors,
-                    fill: false,
-                    tension: 0.1 // rende la linea leggermente curva
-                }, 
-                {
-                    label: "Visite",
-                    backgroundColor: "rgba(0, 0, 0, 0)",
-                    borderColor: "rgba(0, 255, 0, 1)",
-                    data: visits,
-                    fill: false,
-                    tension: 0.1 // rende la linea leggermente curva
-                },
-                {
-                    label: "Pagine visitate",
-                    backgroundColor: "rgba(0, 0, 0, 0)",
-                    borderColor: "rgba(255, 0, 0, 1)",
-                    data: pageviews,
-                    fill: false,
-                    tension: 0.1 // rende la linea leggermente curva
-                }],
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    title: {
-                        display: true,
-                        text: 'Visitors Over Time'
-                    },
-                    legend: {
-                        display: true,
-                        position: 'top'
-                    },
-                    tooltip: {
-                        mode: 'index',
-                        intersect: false,
-                    }
-                },
-            }
-        });
+        // Creazione grafico
+        createChart(info.reverse());
     })
     .catch(error => console.error(error));
